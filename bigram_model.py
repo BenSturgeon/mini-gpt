5d8c926c1dff84654f525b529ae1fccfde5c2fca
Benjamin Sturgeon <benjamin@cape-ai.com>
2023-05-29 17:26:35 +0200

    Added a system to check the difference in outputs when the key and query networks are swapped at runtime.

--- bigram_model.py
+++ bigram_model.py
@@ -1,3 +1,5 @@
+import argparse
+import sys
 import torch
 import torch 
 import torch.nn as nn
@@ -14,12 +16,11 @@ block_size = 128
 n_embd = 192
 n_head =4
 n_layer = 4
-lr = 2e-3
+lr = 3e-3
 dropout = 0.2
 training_iters = 5000
 eval_interval = 300
 eval_iters = 200
-output_length = 10000
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 torch.manual_seed(1337)
 
@@ -65,20 +66,20 @@ def get_batch(split):
     return x,y
 
 @torch.no_grad() # tells pytorch we don't intend to do backprop. saves memory by not saving gradients.
-def estimate_loss(model):
+def estimate_loss(model, reverse=False):
     out = {}
     model.eval()
     for split in ['train', 'val']:
         losses = torch.zeros(eval_iters)
         for iter in range(eval_iters):
             X, Y = get_batch(split)
-            logits, loss = model(X,Y)
+            X, Y = X.to(device), Y.to(device)
+            logits, loss = model(X,Y, reverse=reverse)
             losses[iter] = loss.item()
         out[split] = losses.mean()
     model.train()
     return out
         
-
 class Head(nn.Module):
     """A single self-attention head"""
     def __init__(self, head_size):
@@ -89,10 +90,16 @@ class Head(nn.Module):
         self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, x):
+    def forward(self, x, reverse=False):
         B,T,C = x.shape
-        k = self.key(x)      
-        q = self.query(x)   
+
+        if reverse:
+
+            q = self.key(x)      
+            k = self.query(x)   
+        else:
+            k = self.key(x)      
+            q = self.query(x)   
 
         # To determine the attention of words (more exactly tokens) we use ‘queries’, ‘keys’ and ‘values’.
         # All of them are presented in vectors. 
@@ -115,8 +122,8 @@ class MultiHeadAttention(nn.Module):
         self.proj = nn.Linear(n_embd, n_embd)
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, x):
-        out = torch.cat([h(x) for h in self.heads], dim=-1)
+    def forward(self, x, reverse=False):
+        out = torch.cat([h(x, reverse=reverse) for h in self.heads], dim=-1)
         out = self.dropout(self.proj(out)) # Projection back into the residual pathway
         return out
     
@@ -142,8 +149,9 @@ class Block(nn.Module):
         self.ln1 = nn.LayerNorm(n_embd)
         self.ln2 = nn.LayerNorm(n_embd)
 
-    def forward(self, x):
-        x = x +self.sa(self.ln1(x)) # The adding of the values to x is our residual connections, or skip connections
+    def forward(self, x, reverse=False):
+        ln_x = self.ln1(x)
+        x = x + self.sa(ln_x, reverse= reverse) # The adding of the values to x is our residual connections, or skip connections
         x = x + self.ffwd(self.ln2(x))
         return x
 
@@ -152,18 +160,20 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.blocks = nn.Sequential(*[Block(n_embd, n_head) for _ in range(n_layer)],
-            nn.LayerNorm(n_embd),
-        )
+        self.blocks =  nn.ModuleList([Block(n_embd, n_head) for _ in range(n_layer)])
+        self.ln = nn.LayerNorm(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
-    def forward(self, idx, targets=None):
+    def forward(self, idx, targets=None, reverse=False):
         B,T = idx.shape
 
         tok_emb = self.token_embedding_table(idx)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device))
         x= tok_emb + pos_emb
-        x= self.blocks(x)
+        x = x.to(device)
+        for block in self.blocks:
+            x = block(x, reverse=reverse)
+        x = self.ln(x)
         logits = self.lm_head(x)
 
         if targets == None:
@@ -184,11 +194,12 @@ class BigramLanguageModel(nn.Module):
         return logits, loss
         return logits
     
-    def generate(self, x_input, max_new_tokens):
+    def generate(self, x_input, max_new_tokens, reverse=False):
 
         for _ in range(max_new_tokens):
             reduced_x_input = x_input[:,-block_size:]
-            logits, loss = self(reduced_x_input) # we're not using loss, as we're generating
+            
+            logits, loss = self.forward(reduced_x_input, reverse=reverse) # we're not using loss, as we're generating
 
             next_token = logits[:, -1,:]
 
@@ -200,15 +211,49 @@ class BigramLanguageModel(nn.Module):
 
         return x_input
 
-    def save_model(self, path):
-        torch.save(self.state_dict(), path)
+
+def show_output(model, output_path, num_tokens):
+    context = torch.zeros((1,1), dtype=torch.long, device=device)
+    with open(output_path, 'a') as f:
+        f.write(decode(model.generate(context, max_new_tokens=num_tokens)[0].tolist()) + '\n')
+
+    reversed_average_loss = estimate_loss(model, reverse=True)
+
+    with open(output_path, 'a') as f:
+        f.write(f"""------ 
+
+Continued with keys and queries reversed: 
+
+------ \n""")
+
+    with open(output_path, 'a') as f:
+        f.write(decode(model.generate(context, max_new_tokens=num_tokens, reverse=True)[0].tolist()) + '\n')
+
+def train( model, train_time, output_path, save_path, num_tokens):
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+
+    for iter in range(train_time):
+        if iter % eval_interval == 0 and iter>0:
+            model = model.to(device)
+            averaged_losses = estimate_loss(model)
+            reversed_average_loss = estimate_loss(model, reverse=True)
+
+            with open(output_path, 'a') as f:
+
+                f.write(f"steps: {iter}  train loss:{averaged_losses['train']:.4f}  test loss:{averaged_losses['val']:.4f} reversed loss: {reversed_average_loss['val']:.4f}\n")
+                if save_path is not None:
+                    model.load_state_dict(torch.load(save_path))
+        if iter % 1000 == 0 and iter>0:
+            show_output(model, output_path, num_tokens=num_tokens)
+        
+        xb, yb = get_batch(batch_size)
+        logits, loss = model(xb, yb)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
     
-    def load_model(self, path):
-        self.load_state_dict(torch.load(path))
+    return model
 
-
-import argparse
-import sys
 
 def main():
     parser = argparse.ArgumentParser()
@@ -216,40 +261,34 @@ def main():
     parser.add_argument('--save', type=str, default=None, help='Path to save the trained model')
     parser.add_argument('--train_time', type=int, default=1000, help='Training iterations')
     parser.add_argument('--output_path', type=str, default='log.txt', help='Path to save log')
+    parser.add_argument('--num_tokens', type=int, default=250, help='number of output tokens')
+
 
     args = parser.parse_args()
 
     model = BigramLanguageModel()
     model = model.to(device)
+
+
+
     
     if args.load is not None:
         model.load_state_dict(torch.load(args.load))
-    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+        print(model.load_state_dict(torch.load(args.load)))
 
     with open(args.output_path, 'w') as f:
         f.write(f'Model is on device: {next(model.parameters()).device}\n')
 
-    for iter in range(args.train_time):
-        if iter % eval_interval == 0:
-            averaged_losses = estimate_loss(model)
-            with open(args.output_path, 'a') as f:
-                f.write(f"steps: {iter}  train loss:{averaged_losses['train']:.4f}  test loss:{averaged_losses['val']:.4f}\n")
+    # Train the model
+    if args.train_time > 0:
+        model =train(model, args.train_time, args.output_path, args.save, num_tokens=args.num_tokens)
 
-        xb, yb = get_batch(batch_size)
-        logits, loss = model(xb, yb)
-        optimizer.zero_grad(set_to_none=True)
-        loss.backward()
-        optimizer.step()
-
+    # # save the model
     if args.save is not None:
-        model.save_model(args.model_path)
+        torch.save(model.state_dict(), args.save)
 
-    # Load the model
-    # model.load_state_dict(torch.load(args.model_path))
 
-    context = torch.zeros((1,1), dtype=torch.long, device=device)
-    with open(args.output_path, 'a') as f:
-        f.write(decode(model.generate(context, max_new_tokens=output_length)[0].tolist()) + '\n')
+    show_output(model, args.output_path, num_tokens=args.num_tokens)
 
 if __name__ == "__main__":
     main()
